d69d2fa25746687ffef089eeff79f4bae35f7ada
Oleksii Gulchenko <oleksii.gulchenko@globallogic.com>
2019-01-28 18:18:39 +0200

    Added Mali T860 prebuilts for RK3399 platform
    Signed-off-by: Oleksii Gulchenko <alexey.gulchenko@gmail.com>

---
+++ mali/Android.mk
@@ -0,0 +1,19 @@
+
+ifneq ($(filter rk%, $(TARGET_BOARD_PLATFORM)),)
+ifeq ($(strip $(TARGET_BOARD_PLATFORM_GPU)), mali-t860)
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE_SUFFIX     := .so
+LOCAL_MODULE            := libGLES_mali
+LOCAL_MODULE_CLASS      := SHARED_LIBRARIES
+LOCAL_MULTILIB          := both
+LOCAL_SRC_FILES_arm64   := $(TARGET_BOARD_PLATFORM)/lib64/libGLES_mali.so
+LOCAL_SRC_FILES_arm     := $(TARGET_BOARD_PLATFORM)/lib/libGLES_mali.so
+LOCAL_MODULE_PATH_32    := $(TARGET_OUT_VENDOR)/lib/egl
+LOCAL_MODULE_PATH_64    := $(TARGET_OUT_VENDOR)/lib64/egl
+include $(BUILD_PREBUILT)
+
+endif # $(TARGET_BOARD_PLATFORM_GPU)
+endif # $(TARGET_BOARD_PLATFORM)
