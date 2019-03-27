8cb483793b77d27736ddf00f49da440d601f5df0
Oleksii Gulchenko <alexey.gulchenko@gmail.com>
2019-03-27 14:34:56 +0200

    Added Mali 450 prebuilts for RK3328 platform
    Signed-off-by: Oleksii Gulchenko <alexey.gulchenko@gmail.com>

--- mali/Android.mk
+++ mali/Android.mk
@@ -1,19 +1,23 @@
 
 ifneq ($(filter rk%, $(TARGET_BOARD_PLATFORM)),)
-ifeq ($(strip $(TARGET_BOARD_PLATFORM_GPU)), mali-t860)
 
 LOCAL_PATH := $(call my-dir)
+
+ifneq ($(strip $(TARGET_BOARD_PLATFORM_GPU)), mali-t860)
+ifneq ($(strip $(TARGET_BOARD_PLATFORM_GPU)), mali-450)
+endif
+$(error No libGLES_mali prebuilts for '$(TARGET_BOARD_PLATFORM_GPU)')
+endif
 
 include $(CLEAR_VARS)
 LOCAL_MODULE_SUFFIX     := .so
 LOCAL_MODULE            := libGLES_mali
 LOCAL_MODULE_CLASS      := SHARED_LIBRARIES
 LOCAL_MULTILIB          := both
-LOCAL_SRC_FILES_arm64   := $(TARGET_BOARD_PLATFORM)/lib64/libGLES_mali.so
-LOCAL_SRC_FILES_arm     := $(TARGET_BOARD_PLATFORM)/lib/libGLES_mali.so
+LOCAL_SRC_FILES_arm64   := $(TARGET_BOARD_PLATFORM_GPU)/lib64/libGLES_mali.so
+LOCAL_SRC_FILES_arm     := $(TARGET_BOARD_PLATFORM_GPU)/lib/libGLES_mali.so
 LOCAL_MODULE_PATH_32    := $(TARGET_OUT_VENDOR)/lib/egl
 LOCAL_MODULE_PATH_64    := $(TARGET_OUT_VENDOR)/lib64/egl
 include $(BUILD_PREBUILT)
 
-endif # $(TARGET_BOARD_PLATFORM_GPU)
 endif # $(TARGET_BOARD_PLATFORM)
